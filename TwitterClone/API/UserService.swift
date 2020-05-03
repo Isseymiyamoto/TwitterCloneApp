1c417c307d46006755cb37776c8c0731c0ee072c
IsseiMiyamoto <ants.can.walk@gmail.com>
2020-05-03 11:50:19 +0900

    FollowButtonに呼応するdatabase関連のfunction定義2

--- TwitterClone/API/UserService.swift
+++ TwitterClone/API/UserService.swift
@@ -9,6 +9,9 @@
 import Foundation
 import Firebase
 
+// 型の別名をつけることができるtypealias
+typealias DatabaseCompletion = ((Error?, DatabaseReference) -> Void)
+
 struct UserService {
     static let shared = UserService()
     
@@ -33,11 +36,19 @@ struct UserService {
         }
     }
     
-    func followUser(uid: String, completion: @escaping(Error?, DatabaseReference) -> Void){
+    func followUser(uid: String, completion: @escaping(DatabaseCompletion)){
         guard let currentUid = Auth.auth().currentUser?.uid else { return }
         
         REF_USER_FOLLOWING.child(currentUid).updateChildValues([uid: 1]) { (err, ref) in
             REF_USER_FOLLOWERS.child(uid).updateChildValues([currentUid: 1], withCompletionBlock: completion)
+        }
+    }
+    
+    func unfollowUser(uid: String, completion: @escaping(DatabaseCompletion)){
+        guard let currentUid = Auth.auth().currentUser?.uid else { return }
+        
+        REF_USER_FOLLOWING.child(currentUid).child(uid).removeValue { (err, ref) in
+            REF_USER_FOLLOWERS.child(uid).child(currentUid).removeValue(completionBlock: completion)
         }
     }
     

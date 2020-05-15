3a3386ab3c4998120bfc61516047ed7b5f955bfc
IsseiMiyamoto <ants.can.walk@gmail.com>
2020-05-15 15:39:26 +0900

    upload like notifications

---
+++ TwitterClone/API/NotificationService.swift
@@ -0,0 +1,29 @@
+//
+//  NotificationService.swift
+//  TwitterClone
+//
+//  Created by 宮本一成 on 2020/05/14.
+//  Copyright © 2020 ISSEY MIYAMOTO. All rights reserved.
+//
+
+import Firebase
+
+struct NotificationService {
+    static let shared = NotificationService()
+    
+    func uploadNotification(type: NotificationType, tweet: Tweet? = nil){
+        
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        
+        var values: [String: Any] = ["timestamp": Int(NSDate().timeIntervalSince1970),
+                                     "uid": uid,
+                                     "type": type.rawValue]
+        
+        if let tweet = tweet {
+            values["tweetID"] = tweet.tweetID
+            REF_NOTIFICATIONS.child(tweet.user.uid).childByAutoId().updateChildValues(values)
+        }else {
+            
+        }
+    }
+}

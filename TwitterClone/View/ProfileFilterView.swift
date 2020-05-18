3667c3533f34f1c1c1cd8c07ecc74348327b4b7c
IsseiMiyamoto <ants.can.walk@gmail.com>
2020-05-18 15:45:42 +0900

    selecting filter and delegating action with protocols

--- TwitterClone/View/ProfileFilterView.swift
+++ TwitterClone/View/ProfileFilterView.swift
@@ -11,7 +11,7 @@ import UIKit
 private let reuseIdentifier = "ProfileFilterCell"
 
 protocol ProfileFilterViewDelegate: class {
-    func filterView(_ view: ProfileFilterView, didSelect indexPath: IndexPath)
+    func filterView(_ view: ProfileFilterView, didSelect index: Int)
 }
 
 class ProfileFilterView: UIView {
@@ -92,7 +92,7 @@ extension ProfileFilterView: UICollectionViewDelegate{
             self.underlineView.frame.origin.x = xPosition
         }
         
-        delegate?.filterView(self, didSelect: indexPath)
+        delegate?.filterView(self, didSelect: indexPath.row)
     }
 }
 
--- TwitterClone/View/ProfileHeader.swift
+++ TwitterClone/View/ProfileHeader.swift
@@ -11,6 +11,7 @@ import UIKit
 protocol ProfileHeaderDelegate: class {
     func handleDismissal()
     func handleEditProfile(_ header: ProfileHeader)
+    func didSelect(filter: ProfileFilterOptions)
 }
 
 class ProfileHeader: UICollectionReusableView{
@@ -200,7 +201,8 @@ class ProfileHeader: UICollectionReusableView{
 //MARK: - ProfileFilterViewDelegate
 
 extension ProfileHeader: ProfileFilterViewDelegate{
-    func filterView(_ view: ProfileFilterView, didSelect indexPath: IndexPath) {
-
+    func filterView(_ view: ProfileFilterView, didSelect index: Int) {
+        guard let filter = ProfileFilterOptions(rawValue: index) else { return }
+        delegate?.didSelect(filter: filter)
     }
 }

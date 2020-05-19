b5da2f61e6f61b5f7c44269a6692a2b457028b82
IsseiMiyamoto <ants.can.walk@gmail.com>
2020-05-19 16:26:38 +0900

    show tweets from followed users only and sort by date

--- TwitterClone/Controllers/FeedController.swift
+++ TwitterClone/Controllers/FeedController.swift
@@ -38,12 +38,25 @@ class FeedController: UICollectionViewController{
         navigationController?.navigationBar.isHidden = false
     }
     
+    // MARK: - Selectors
+    
+    @objc func handleRefresh(){
+        fetchTweets()
+    }
+    
     // MARK: - API
     
     func fetchTweets(){
+        collectionView.refreshControl?.beginRefreshing()
+        
         TweetService.shared.fetchTweets { (tweets) in
+            
             self.tweets = tweets
             self.checkIfUSerLikedTweets(self.tweets)
+            
+            self.tweets = tweets.sorted(by: { $0.timestamp > $1.timestamp })
+            
+            self.collectionView.refreshControl?.endRefreshing()
         }
     }
     
@@ -53,6 +66,7 @@ class FeedController: UICollectionViewController{
                 guard didLike == true else { return }
                 
                 self.tweets[index].didLike = true
+                
             }
         }
     }
@@ -69,6 +83,10 @@ class FeedController: UICollectionViewController{
         imageView.contentMode = .scaleAspectFit
         imageView.setDimensions(width: 44, height: 44)
         navigationItem.titleView = imageView
+        
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(handleRefresh), for: .valueChanged)
+        collectionView.refreshControl = refreshControl
     }
     
     func configureLeftBarButton(){
